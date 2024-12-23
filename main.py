42fd26ceee0d9e62e28e35a3eeeb4ed33f48a7a7
Steven McKenzie <sdmckenzie04@gmail.com>
2024-12-23 13:47:01 -0500

    Add function to print character count as report.

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def main():
     print(word_count)
 
     characters = countCharacters(book_content)
-    print(characters)
+    # print(characters)
+    printReport(characters)
 
 
 def openBook(book_path):
@@ -34,6 +35,16 @@ def countCharacters(document):
         else:
             char_count[char] = 1
     return char_count
+
+
+def printReport(char_dict):
+    sorted_dict = dict(
+        sorted(char_dict.items(), key=lambda item: item[1], reverse=True))
+    for entry in sorted_dict:
+        if entry.isalpha():
+            print(f"The {entry} character was found {char_dict[entry]} times")
+        else:
+            pass
 
 
 main()
