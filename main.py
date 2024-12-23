65bc5ccc7afbd9a5eccea246da472218d9ce579d
Steven McKenzie <sdmckenzie04@gmail.com>
2024-12-22 22:13:12 -0500

    Adjust main.py to print word count instead of content.

--- main.py
+++ main.py
@@ -3,14 +3,23 @@
 def main():
     book_path = "books/frankenstein.txt"
     book_content = openBook(book_path)
-
-    print(book_content)
+    word_count = getWordcount(book_content)
+    print("The total word count is:\n")
+    print(word_count)
 
 
 def openBook(book_path):
     with open(book_path) as f:
         file_contents = f.read()
     return file_contents
+
+
+def getWordcount(document):
+    word_list = document.split()
+    counter = 0
+    for word in word_list:
+        counter += 1
+    return counter
 
 
 main()
